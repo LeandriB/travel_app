ef92e79ff716a619e8c21651bdc1739a83ed2828
Leandri Beavis <leandribeavis@Jonathans-iMac.local>
2020-10-16 14:31:24 -0400

    Updated POST route for server

--- src/server/server.js
+++ src/server/server.js
@@ -4,10 +4,6 @@ let tripData = {};
 // Dependencies
 var path = require('path');
 
-// Used to keep API key private
-const dotenv = require('dotenv');
-dotenv.config();
-
 // Require Express to run server and routes
 const express = require('express');
 // Start up an instance of an app
@@ -17,7 +13,7 @@ app.use(express.static('dist'));
 
 // Body Parser as middleware
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
@@ -26,60 +22,31 @@ app.use(cors());
 
 // Require node-fetch
 const fetch = require('node-fetch');
+const { response } = require('express');
 
 // Designates what port the app will listen to for incoming requests
-const port = 8080; // Change PORT?
-// call to debug
-app.listen(port, function() {
-    console.log(`Travel app is listening on  ${port}!`)
-});
+const port = 5500;
+app.listen(port, listening);
+// Callback to debug
+function listening() {
+  console.log(`Travel app listening on port ${port}!`);
+};
 
 // GET Route
-app.get('/', function(request, response) {
-    response.sendFile('dist/index.html')
-});
+app.get('/', function (request, response) {
+  response.sendFile('dist/index.html')
+})
 
-// Add POST routes
+// POST Route
+app.post('/add', tripInfo);
 
-// POST route for geonames
-app.post('/geonames', geonames);
-
-async function geonames(request, response) {
-    geonamesData = {
-        latitude: request.body.lat,
-        longitude: request.body.lon,
-    };
-    tripData.push(geonamesData);
-    console.log("User input: ", tripData);
-    response.send(tripData);
-};
-
-// POST route for wethaerbit
-
-app.post('/weather', weatherbit);
-
-async function weatherbit(request, response) {
-    weatherbitData = {
-        high: request.body.high,
-        low: request.body.low,
-        description: request.body.description,
-    };
-    tripData.push(weatherbitData);
-    console.log("User input: ", tripData);
-    response.send(tripData);
-};
-
-// POST route for pixabay
-
-app.post('/image', pixabay);
-
-async function pixabay(request, response) {
-    pixabayData = {
-        image: request.body.image,
-    };
-    tripData.push(pixabayData);
-    console.log("User input: ", tripData);
-    response.send(tripData);
-};
-
-module.exports = server;
+function tripInfo(request, response) {
+  tripData['city'] = request.body.depCity;
+  tripData['arrCity'] = request.body.arrCity;
+  tripData['depDate'] = request.body.depDate;
+  tripData['weather'] = request.body.weather;
+  tripData['summary'] = request.body.weather.description;
+  tripData['daysLeft'] = request.body.daysLeft;
+  console.log("User Input: ", tripData)
+  response.send(tripData);
+}
