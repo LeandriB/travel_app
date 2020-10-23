a6d57fa577d9c30286cde4c8172353071b89cd6a
Leandri Beavis <leandribeavis@Jonathans-iMac.local>
2020-10-23 19:59:56 -0400

    Added alert

--- src/client/js/inputCheck.js
+++ src/client/js/inputCheck.js
@@ -1,9 +1,11 @@
+import swal from "sweetalert";
+
 function checkInput(departing, destination) {
    let urlRGEX = /^[a-zA-Z\s]{0,255}$/;
    if (urlRGEX.test(departing) && urlRGEX.test(destination)) {
      return
    } else {
-     alert("Please enter a valid name");
+     swal("Invalid input");
    }
  }
  
