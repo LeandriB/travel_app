9007875ebb685dcbac1bf8e99af912a512c88694
LeandriB <leandrikuyk@gmail.com>
2020-10-25 14:10:35 -0400

    Added test for input

--- src/client/js/inputCheck.js
+++ src/client/js/inputCheck.js
@@ -1,13 +1,12 @@
 import swal from "sweetalert";
 
 function checkInput(departing, destination) {
-   let urlRGEX = /^[a-zA-Z\s]{0,255}$/;
-   if (urlRGEX.test(departing) && urlRGEX.test(destination)) {
-     return
-   } else {
-     swal("Invalid input");
-   }
- }
- 
- export { checkInput }
- 
+    let urlRGEX = /^[a-zA-Z\s]{0,255}$/;
+    if (urlRGEX.test(departing) && urlRGEX.test(destination)) {
+      return
+    } else {
+      swal("Invalid input");
+    }
+  }
+
+export { checkInput }
